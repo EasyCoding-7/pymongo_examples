bc505497bc4d3dd5192f40b8b4741e1afd21be8f
GoodayTH <taehyeongkim77@gmail.com>
2022-03-03 10:27:49 +0900

    배우 상세정보

--- pymongo_1/pymongo_1/pymongo_2.py
+++ pymongo_1/pymongo_1/pymongo_2.py
@@ -76,6 +76,7 @@ for actor in actors:
         print(re.sub('<.*?>','',str(actor_value)))
 """
 
+"""
 # 배우 상세정보 추출 list, dict 관리
 actors_detail_info_list = list()
 
@@ -103,12 +104,61 @@ print(actors_detail_info_list)
 # 배우 흥행지수 뽑기
 actors = soup.select('li.people_li div.name')
 hits = soup.select('ul.num_info > li > strong')
+movies = soup.select('ul.mov_list')
 
 # print(actors)
 # print(hits)
 
 for index, actor in enumerate(actors):
     # print(actor)
-    print(re.sub('\(\w*\)','',actor.text))
+    # print(re.sub('\(\w*\)','',actor.text))
+    actor_name = re.sub('\(\w*\)','',actor.text)
     # print(hits[index])
-    print(int(hits[index].text.replace(',', '')))
+    # print(int(hits[index].text.replace(',', '')))
+    actor_hits = int(hits[index].text.replace(',', ''))
+    movie_titles = movies[index].select('li a span')
+    movie_titles_list = list()
+    for movie in movie_titles:
+        # print(movie.text)
+        movie_titles_list.append(movie.text)
+    print(movie_titles_list)
+"""
+
+# 배우 상세정보 추출 list, dict 관리
+actors_detail_info_list = list()
+
+# 배우 흥행지수 뽑기
+actors = soup.select('li.people_li div.name')
+hits = soup.select('ul.num_info > li > strong')
+movies = soup.select('ul.mov_list')
+
+for index, actor in enumerate(actors):
+    # 배우이름(actor_name), hit점수(actor_hits), 대표작 리스트(movie_titles_list) 파싱
+    actor_name = re.sub('\(\w*\)','',actor.text)
+    actor_hits = int(hits[index].text.replace(',', ''))
+    movie_titles = movies[index].select('li a span')
+    movie_titles_list = list()
+    for movie in movie_titles:
+        movie_titles_list.append(movie.text)
+
+    # 배우 상세정보(actor_url)
+    actor_url = 'http://www.cine21.com' + actor.select_one('a').attrs['href']
+    response_actor = requests.get(actor_url)
+    soup_actor = BeautifulSoup(response_actor.content, 'html.parser')
+    actor_info = soup_actor.select_one('ul.default_info')
+    actor_details = actor_info.select('li')
+
+    actor_info_dict = dict()
+    actor_info_dict['배우이름'] = actor_name
+    actor_info_dict['hits'] = actor_hits
+    actor_info_dict['출연작'] = movie_titles_list
+
+    for item in actor_details:
+        actor_item_filed = item.select_one('span.tit').text
+        actor_value = re.sub('<span.*?>.*?</span>','',str(item))
+        actor_item_value = re.sub('<.*?>','',str(actor_value))
+        actor_info_dict[actor_item_filed] = actor_item_value
+
+    actors_detail_info_list.append(actor_info_dict)
+
+print(actors_detail_info_list)
